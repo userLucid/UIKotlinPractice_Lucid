62e5a68e353385703bdfda37365500ccaa1952bd
windows <nvpz1598@gmail.com>
2021-07-18 02:27:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/uikotlinpractice_lucid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.uikotlinpractice_lucid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
